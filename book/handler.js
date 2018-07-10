55bcaef263d5faeecf1a3077863ed8c79c66176e
Jardel Martins <jardelvdm@gmail.com>
2018-07-10 17:06:46 -0300

    Adding Linkedin script to variable

--- book/handler.js
+++ book/handler.js
@@ -56,6 +56,21 @@ const adrollScriptTrack = `
   </script>
 `;
 
+const linkedinScriptTrack = `
+  <script type="text/javascript">
+          _linkedin_data_partner_id = "328324";
+          </script><script type="text/javascript">
+          (function(){var s = document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);})();
+  </script>
+  <noscript>
+          <img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=328324&fmt=gif" />
+  </noscript>
+`;
+
 require(["gitbook", "jQuery"], function(gitbook, $) {
 
     gitbook.events.bind("start", function(e, config) {
