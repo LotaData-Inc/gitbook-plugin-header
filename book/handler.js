cf812883c489f20aa7698f81458eb458db902996
Jardel Martins <jardelvdm@gmail.com>
2019-01-08 16:53:53 -0200

    Adding support to PIXEL track

--- book/handler.js
+++ book/handler.js
@@ -102,6 +102,29 @@ const analyticsScriptTrack = `
   </script>
 `;
 
+const analyticsPixelTrack = `
+    <script type="text/javascript">
+       adroll_adv_id = "33RN5EGX5RHFNBW27CA5GF";
+       adroll_pix_id = "PWRUHUNWGBFGDE5T5PUTXI";
+
+       (function () {
+           var _onload = function(){
+               if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+               if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+               var scr = document.createElement("script");
+               var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+               scr.setAttribute(‘async’, ‘true’);
+               scr.type = "text/javascript";
+               scr.src = host + "/j/roundtrip.js";
+               ((document.getElementsByTagName(‘head’) || [null])[0] ||
+                   document.getElementsByTagName(‘script’)[0].parentNode).appendChild(scr);
+           };
+           if (window.addEventListener) {window.addEventListener(‘load’, _onload, false);}
+           else {window.attachEvent(‘onload’, _onload)}
+       }());
+    </script>
+`;
+
 require(["gitbook", "jQuery"], function(gitbook, $) {
 
     gitbook.events.bind("start", function(e, config) {
@@ -115,7 +138,7 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
       }
 
       function adsScripts() {
-        return [adrollScriptTrack,linkedinScriptTrack,facebookScriptTrack,analyticsScriptTrack].join('');
+        return [adrollScriptTrack,linkedinScriptTrack,facebookScriptTrack,analyticsScriptTrack,analyticsPixelTrack].join('');
       }
       // Get configuration.
       var headerTitle = 'NEW HEADER';
